0cd1d2049e554558760c8bdedd304fc708a3dbc7
Natan Graffitti <125401756+NatanGraffitti@users.noreply.github.com>
2024-07-24 17:46:01 -0300

    Create BancoSpringBootApplication.java

---
+++ BancoSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.example.bancospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BancoSpringBootApplication.class, args);
+    }
+}
